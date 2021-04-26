641f35563501eb0b76906654957a9da4091dc77f
Sasha Volokh <volokh@usc.edu>
2021-04-26 14:00:02 -0700

    update copy script

--- docker/demo/copy.bash
+++ docker/demo/copy.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp -r ../../../java-concolic/instrumenter/instrumentation .
+rm instrumentation/.gitignore
 cp ../../../java-concolic/instrumenter/target/java-concolic-1.0-SNAPSHOT.jar .
 cp ../../../java-concolic/concolic/concolic.py .
